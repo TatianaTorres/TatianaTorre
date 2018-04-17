6c1c39e38a1e5ab85f6c3e2bd2d3691ca6327171
Administrador <Administrador@172.16.17.137>
2018-04-17 14:53:03 -0600

    programa con menu

---
+++ src/exaprac2/TatianaTorres.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exaprac2;
+
+/**
+ *
+ * @author Administrador
+ */
+public class TatianaTorres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int op;
+        
+         while(op!=3)
+        {
+            System.out.print("..........agregar producto");
+            System.out.print("............calcular total"); 
+            System.out.print(".............salir");
+           
+            op=Integer.parseInt(JOptionPane.showInputDialog("ingresar opcion"));
+            int Op = 0;
+            
+            switch(Op)
+            {
+                case 1:
+                {
+                    System.out.print("..........agregar producto");
+                    
+                }
+                      int i;
+                      
+                      
+                      
+                }
+            }
+            break;
+                    case 2:
+                    
+                    System.out.print("..........lista de productos");
+                    
+        
+            
+            break;
+            
+            case 3:
+            {
+            System.out.print(".............salir");
+           }
+           break;
+           
+    
+            
+                    
+                    
+                    
+                    
+                   
+        
+       
+                    
+    }
+    
+}
